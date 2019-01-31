c658ae885ae8f31a59525c2d3483b7547a64a350
Kelle Connolly <kelle.connolly@gmail.com>
2019-01-31 13:23:42 +1100

    Refactor gordon

--- src/gordonWaltersKoru.js
+++ src/gordonWaltersKoru.js
@@ -1,96 +1,61 @@
 const canvasSketch = require("canvas-sketch");
 const random = require("canvas-sketch-util/random");
+const palettes = require("nice-color-palettes");
 
+const goodSeeds = [
+  93329,
+  5028,
+  98293,
+  816054,
+  102449,
+  575738,
+  165791,
+  609544,
+  40808,
+  947080,
+  799294,
+  647391,
+  861120,
+  846607,
+  515460,
+  587869
+];
+
+// random.setSeed(random.pick(goodSeeds));
 random.setSeed(random.getRandomSeed());
-// console.log(random.getSeed());
+console.log(random.getSeed());
 
 const settings = {
   dimensions: [2048, 2048],
   suffix: `-seed-${random.getSeed()}`
 };
 
-const decideIfCircle = (rowSquares, count, rowIndex, columnIndex, rowAbove) => {
-  // don't be on the first or last, top or bottom
-  if (
-    rowIndex === 0 ||
-    rowIndex === count - 1 ||
-    columnIndex < 2 ||
-    columnIndex > count - 3
-  )
-    return false;
+const colorCount = 2;
+const palette = random.shuffle(random.pick(palettes).slice(0, colorCount));
 
-  const previousSquare = rowIndex[columnIndex - 1] || {};
-  const previousPreviousSquare = rowIndex[columnIndex - 2] || {};
-
-  if (
-    previousSquare.circle ||
-    previousSquare.skip ||
-    previousSquare.cap ||
-    previousPreviousSquare.circle ||
-    previousPreviousSquare.skip ||
-    previousPreviousSquare.cap
-  )
-    return false;
-
-  const rowAboveSquare = (rowAbove && rowAbove.squares[columnIndex]) || {};
-  const rowAbovePreviousSquare =
-    (rowAbove && rowAbove.squares[columnIndex - 1]) || {};
-  if (
-    rowAboveSquare.circle ||
-    rowAboveSquare.skip ||
-    rowAboveSquare.cap ||
-    rowAbovePreviousSquare.circle ||
-    rowAbovePreviousSquare.skip ||
-    rowAbovePreviousSquare.cap
-  )
-    return false;
-  return random.value() > 0.95;
-};
-
-const decideIfSkipped = (rowSquares, columnIndex) => {
-  return rowSquares[columnIndex - 1] && rowSquares[columnIndex - 1].circle;
-};
-
-const decideIfCapped = (rowSquares, columnIndex) => {
-  return rowSquares[columnIndex - 1] && rowSquares[columnIndex - 1].skip;
-};
-
-const decideIfBuffer = (rowSquares, columnIndex) => {
-  return rowSquares[columnIndex - 1] && rowSquares[columnIndex - 1].cap;
-};
-
-const createGrid = (gridWidth, gridHeight, offsetX, offsetY) => {
-  const count = 20;
+const createGrid = (canvasWidth, canvasHeight, count, margin) => {
+  const gridHeight = canvasHeight - margin * 2;
+  const gridWidth = canvasWidth - margin * 2;
   const rows = [];
 
   for (let rowIndex = 0; rowIndex < count; rowIndex++) {
     const row = {
-      x: Math.ceil(offsetX),
-      y: Math.ceil(offsetY + rowIndex * gridHeight / count),
+      x: Math.ceil(margin),
+      y: Math.ceil(margin + rowIndex * gridHeight / count),
       width: Math.ceil(gridWidth),
       height: Math.ceil(gridHeight / count),
-      backgroundColor: rowIndex % 2 ? "lightblue" : "black",
+      backgroundColor: rowIndex % 2 ? palette[0] : palette[1],
       squares: []
     };
 
     for (let columnIndex = 0; columnIndex < count; columnIndex++) {
       row.squares.push({
         position: [
-          Math.ceil(offsetX + gridWidth / count * columnIndex),
-          Math.ceil(offsetY + gridHeight / count * rowIndex)
+          Math.ceil(margin + gridWidth / count * columnIndex),
+          Math.ceil(margin + gridHeight / count * rowIndex)
         ],
         w: Math.ceil(gridWidth / count),
-        h: Math.ceil(gridHeight / count),
-        color: rowIndex % 2 ? "black" : "lightblue",
-        circle: decideIfCircle(
-          row.squares,
-          count,
-          rowIndex,
-          columnIndex,
-          rows[rowIndex - 1]
-        ),
-        skip: decideIfSkipped(row.squares, columnIndex),
-        cap: decideIfCapped(row.squares, columnIndex)
+        circle: random.value() > 0.8
       });
     }
     rows.push(row);
@@ -100,32 +65,62 @@ const createGrid = (gridWidth, gridHeight, offsetX, offsetY) => {
 };
 
 const sketch = () => {
-  const margin = 100;
   return ({ context, width, height }) => {
-    const croppedWidth = width - margin * 2;
-    const croppedHeight = height - margin * 2;
-    const points = createGrid(croppedWidth, croppedHeight, margin, margin);
+    const count = 8;
+    const numberOfFauxRows = count * 4 + 2;
+    const margin = height / numberOfFauxRows;
+    const points = createGrid(width, height, count, margin);
 
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
-    points.forEach(row => {
+
+    for (let i = 0; i < numberOfFauxRows; i++) {
       context.beginPath();
-      context.fillStyle = row.backgroundColor;
-      context.rect(row.x, row.y, row.width, row.height);
+      context.fillStyle = i % 2 ? palette[0] : palette[1];
+      context.rect(0, i * margin, width, margin);
       context.fill();
+    }
+
+    points.forEach(row => {
       row.squares.forEach(point => {
-        const { position: [x, y], w, h, color, circle, cap, skip } = point;
-        if (!skip) {
+        const { position: [x, y], w, color, circle } = point;
+        context.beginPath();
+        context.fillStyle = color;
+        if (circle) {
+          const quarter = w / 4;
+          const radius = quarter - 10;
+          const row = Math.round(random.range(1, 3));
+          const rowOffset = row * quarter;
           context.beginPath();
-          context.fillStyle = color;
-          if (circle) context.arc(x, y + 10, w - 10, 0, 2 * Math.PI);
-          if (!circle && !cap) context.rect(x, y, w, h);
-          if (cap) {
-            context.rect(x, y, w, h);
-            context.arc(x, y + 10, w - 10, 0, 2 * Math.PI);
-          }
+          context.fillStyle = row % 2 ? palette[1] : palette[0];
+          context.rect(x + quarter, y + rowOffset, w / 2, w / 2);
+          context.fill();
+
+          context.beginPath();
+          context.fillStyle = row % 2 ? palette[0] : palette[1];
+          context.arc(
+            x + quarter,
+            y + quarter + rowOffset + 10,
+            radius,
+            0,
+            2 * Math.PI
+          );
+          context.arc(
+            x + quarter * 3,
+            y + quarter + rowOffset + 10,
+            radius,
+            0,
+            2 * Math.PI
+          );
+
           context.fill();
         }
+
+        // helper grid
+        // context.beginPath();
+        // context.strokeStyle = "black";
+        // context.rect(x, y, w, w);
+        // context.stroke();
       });
     });
   };
