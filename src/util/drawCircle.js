7a911e536fa349a85aa157ab6d04b819a65fe41a
Kelle <kelec@proton.me>
2026-03-17 15:44:30 +1100

    remove old versions

--- src/util/drawCircle.js
+++
@@ -1,6 +0,0 @@
-module.exports = (circle, context) => {
-  context.beginPath();
-  if (circle.color) context.fillStyle = circle.color;
-  context.arc(circle.x, circle.y, circle.radius, 0, Math.PI * 2);
-  context.fill();
-};
--- src/util/isPointWithinCircle.js
+++
@@ -1,9 +0,0 @@
-const isPointWithinCircle = (point, circleMask) => {
-  var a = point.radius + circleMask.radius;
-  var x = point.x - circleMask.x;
-  var y = point.y - circleMask.y;
-
-  return a >= Math.sqrt(x * x + y * y);
-};
-
-module.exports = isPointWithinCircle;
